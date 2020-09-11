7a5b1b6bc2f5888da53df8f95f795c6337e65334
kirainmoe <i@kirainmoe.com>
2020-09-11 10:54:15 +0800

    prepare for github workflow

--- recent.py
+++ recent.py
@@ -10,22 +10,6 @@ usage: python recent.py [username] [password]
 or: python recent.py [cookie:SAAS_U]
 '''
 
-username = ""
-password = ""
-cookie = ""
-use_cookie = False
-
-if len(sys.argv) == 2:
-    cookie = sys.argv[1]
-    use_cookie = True
-elif len(sys.argv) == 3:
-    username = sys.argv[1]
-    password = sys.argv[2]
-else:
-    print("Get recent daily health report result.")
-    print("Usage: python recent.py [username] [password]")
-    print("   or: python recent.py [cookie:SAAS_U]")
-    sys.exit(1)
 
 http_header = {
     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKit/537.36 (KHTML, like Gecko) '
@@ -33,80 +17,110 @@ http_header = {
     'Referer': 'https://xmuxg.xmu.edu.cn/app/221'
 }
 
-# create session
-session = requests.Session()
 
-# login
-login(session, username, password, cookie, use_cookie, http_header)
+def check_recent(username, password, cookie, use_cookie):
+    # create session
+    session = requests.Session()
 
-# get record business id
-try:
-    get_business_url = 'https://xmuxg.xmu.edu.cn/api/app/221/business/now'
-    resp = session.get(get_business_url, headers=http_header).text
-    business_id = json.loads(resp)['data'][0]['business']['id']
-except KeyError:
-    print(json.dumps({
-        "status": "failed",
-        "reason": "auth failed (incorrect auth info or server is down)"
-    }, indent=4))
-    sys.exit(1)
+    # login
+    login(session, username, password, cookie, use_cookie, http_header)
 
-# get recent checkin status
-recent_url = "https://xmuxg.xmu.edu.cn/api/formEngine/business/%s/myFormInstance" % str(business_id)
-resp = session.get(recent_url, headers=http_header).text
-res_json = json.loads(resp)
-form_data = res_json['data']['formData']
-owner = res_json['data']['owner']['name']
+    # get record business id
+    try:
+        get_business_url = 'https://xmuxg.xmu.edu.cn/api/app/221/business/now'
+        resp = session.get(get_business_url, headers=http_header).text
+        business_id = json.loads(resp)['data'][0]['business']['id']
+    except KeyError:
+        return ({
+            "status": "failed",
+            "reason": "auth failed (incorrect auth info or server is down)"
+        }, 1)
 
-# get recent result
-record_data = []
-for item in form_data:
-    if "打卡详细情况" in item['title']:
-        record_data = item['value']['tableValue']
-        break
+    # get recent checkin status
+    recent_url = "https://xmuxg.xmu.edu.cn/api/formEngine/business/%s/myFormInstance" % str(business_id)
+    resp = session.get(recent_url, headers=http_header).text
+    res_json = json.loads(resp)
+    form_data = res_json['data']['formData']
+    owner = res_json['data']['owner']['name']
 
-record_set = []
-for item in record_data:
-    raw = item['rowData']
-    record = {
-        "date": None,
-        "heat": None,
-        "promise": None,
-        "syndrome": None
+    # get recent result
+    record_data = []
+    for item in form_data:
+        if "打卡详细情况" in item['title']:
+            record_data = item['value']['tableValue']
+            break
+
+    record_set = []
+    for item in record_data:
+        raw = item['rowData']
+        record = {
+            "date": None,
+            "heat": None,
+            "promise": None,
+            "syndrome": None
+        }
+        for v in raw:
+            if "日期" in v['title']:
+                record['date'] = v['value']['stringValue']
+            elif "体温" in v['title']:
+                record['heat'] = v['value']['stringValue']
+            elif '本人承诺' in v['title']:
+                record['promise'] = v['value']['stringValue']
+            elif '症状' in v['title']:
+                record['syndrome'] = v['value']['stringValue']
+        record_set.append(record)
+
+    # get today's modification log
+
+    # get form id
+    now_url = "https://xmuxg.xmu.edu.cn/api/app/214/business/now"
+    resp = session.get(now_url).text
+    form_id = str(json.loads(resp)['data'][0]['business']['id'])
+    form_begin = json.loads(resp)['data'][0]['business']['name']
+    cur_time = time.strftime("%Y-%m-%d",  time.localtime())
+
+    form_instance_url = "https://xmuxg.xmu.edu.cn/api/formEngine/business/%s/myFormInstance" % form_id
+    resp = session.get(form_instance_url, headers=http_header).text
+    form_json = json.loads(resp)["data"]
+    instance_id = form_json["id"]
+
+    changelog_url = "https://xmuxg.xmu.edu.cn/api/formEngine/formInstances/%s/changeLogs?playerId=owner&businessId=%s" \
+                    % (instance_id, form_id)
+    log_text = session.get(changelog_url).text
+    log_json = json.loads(log_text)['data']['logs']
+    
+    status_url = "https://xmuxg.xmu.edu.cn/api/schoolcustom/qrCode"
+    status_text = session.get(status_url).text
+    status_json = json.loads(status_text)['data']    
+
+    result = {
+        "owner": owner,
+        "today": len(log_json) > 0 and cur_time == form_begin,
+        "days": status_json["clockDay"],
+        "color": status_json["color"],
+        "payload": record_set
     }
-    for v in raw:
-        if "日期" in v['title']:
-            record['date'] = v['value']['stringValue']
-        elif "体温" in v['title']:
-            record['heat'] = v['value']['stringValue']
-        elif '本人承诺' in v['title']:
-            record['promise'] = v['value']['stringValue']
-        elif '症状' in v['title']:
-            record['syndrome'] = v['value']['stringValue']
-    record_set.append(record)
+    return (result, 0)
 
-# get today's modification log
 
-# get form id
-now_url = "https://xmuxg.xmu.edu.cn/api/app/214/business/now"
-resp = session.get(now_url).text
-form_id = str(json.loads(resp)['data'][0]['business']['id'])
-form_begin = json.loads(resp)['data'][0]['business']['name']
-cur_time = time.strftime("%Y-%m-%d",  time.localtime())
+if __name__ == "__main__":
+    username = ""
+    password = ""
+    cookie = ""
+    use_cookie = False
 
-form_instance_url = "https://xmuxg.xmu.edu.cn/api/formEngine/business/%s/myFormInstance" % form_id
-resp = session.get(form_instance_url, headers=http_header).text
-form_json = json.loads(resp)["data"]
-instance_id = form_json["id"]
-
-changelog_url = "https://xmuxg.xmu.edu.cn/api/formEngine/formInstances/%s/changeLogs?playerId=owner&businessId=%s" \
-                % (instance_id, form_id)
-log_text = session.get(changelog_url).text
-log_json = json.loads(log_text)['data']['logs']
-
-result = {
-    "owner": owner,
-    "today": len(log_json) > 0 and cur_time == form_begin,
-    "payload": record_set
-}
-print(json.dumps(result, ensure_ascii=False, indent=4))
+    if len(sys.argv) == 2:
+        cookie = sys.argv[1]
+        use_cookie = True
+    elif len(sys.argv) == 3:
+        username = sys.argv[1]
+        password = sys.argv[2]
+    else:
+        print("Get recent daily health report result.")
+        print("Usage: python recent.py [username] [password]")
+        print("   or: python recent.py [cookie:SAAS_U]")
+        sys.exit(1)
+    
+    response, status = check_recent(username, password, cookie, use_cookie)
+    print(json.dumps(response, indent=4, ensure_ascii=False))
+    sys.exit(status)
---
+++ workflow.py
@@ -0,0 +1,75 @@
+import requests
+import sys
+import os
+
+from checkin import health_report
+from recent import check_recent
+
+username = ""
+password = ""
+
+
+def report_with_server_chan(flag, reason="", success=""):
+    try:
+        server_chan_secret = os.environ["server_chan_secret"]
+        push_url = "https://sc.ftqq.com/" + server_chan_secret + ".send"
+        if flag:
+            result_text = "今日打卡操作已成功！" + success
+        else:
+            result_text = "今日打卡操作没有成功，请手动完成打卡。错误细节：" + reason
+
+        session = requests.Session()
+        session.post(push_url, {
+            "text": "打卡操作提醒",
+            "description": result_text
+        })
+    except KeyError:
+        print("Cannot report with Server-Chan: secret_key not set")
+        return
+    except Exception:
+        print("Cannot report with Server-Chan: unknown error")
+        return
+
+
+try:
+    username = os.environ["xmu_username"]
+    password = os.environ["xmu_password"]
+except KeyError:
+    reason = "You must provide a valid username & password to log in xmuxg.xmu.edu.cn！"
+    print(reason)
+    report_with_server_chan(False, reason)
+    sys.exit(1)
+
+try:
+    today_log, status = check_recent(username, password)
+    if status == 0 and today_log.today:
+        print("Already reported today :)")
+        sys.exit(0)
+
+    response, status = health_report(username, password)
+    if status != 0:
+        print("Report error, reason: " + response.reason)
+        report_with_server_chan(False, response.reason)
+        sys.exit(1)
+
+    today_log, status = check_recent(username, password)
+    if status == 0:
+        if today_log.today:
+            print("Automatically reported successfully!")
+            success_info = "当前连续打卡" + str(today_log.days) + "天，健康码为" + str(today_log.color) + "码！"
+            report_with_server_chan(True, success=success_info)
+            sys.exit(0)
+        else:
+            print("Automatically reported failed.")
+            reason = "System rejected the health-report request."
+            report_with_server_chan(False, reason)
+            sys.exit(1)
+    else:
+        report_with_server_chan(False, "Internal server error")
+        sys.exit(1)
+
+except Exception:
+    reason = "Error occurred while sending the report request."
+    print(reason)
+    report_with_server_chan(False, reason)
+    sys.exit(1)
